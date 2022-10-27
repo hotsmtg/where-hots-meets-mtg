633e134e99ec259532dea1d5979fab135912c380
hotsmtg <hotsmtg@gmail.com>
2022-10-27 14:11:52 -0400

    Added flip-card on preview

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ function getCommanderById(id) {
     );
 }
 
+function getCommanderByName(name) {
+    return commanders.filter(
+        function (commanders) {
+            return commanders.name == name;
+        }
+    );
+}
+
+
 function selectRandomCommanders(numberOfCommanders) {
 
     let selectedCommanders = [];
@@ -53,12 +62,12 @@ function selectRandomCommanders(numberOfCommanders) {
 
         let validCandidate = (
             candidate[0] &&
-            candidate[0].name != "Artanis" && 
-            candidate[0].name != "ChoGall" && 
-            candidate[0].name != "Hogger" && 
-            candidate[0].name != "Mei" && 
-            candidate[0].name != "Sgt. Hammer" && 
-            candidate[0].name != "Tassadar" && 
+            candidate[0].name != "Artanis" &&
+            candidate[0].name != "ChoGall" &&
+            candidate[0].name != "Hogger" &&
+            candidate[0].name != "Mei" &&
+            candidate[0].name != "Sgt. Hammer" &&
+            candidate[0].name != "Tassadar" &&
             candidate[0].name != "The Lost Vikings"
         )
 
@@ -84,7 +93,7 @@ function generateFeaturedGallery(gallery, numberOfCards) {
         galleryCard.setAttribute('id', 'hs-' + selectedCommanders[i].id)
 
         //set-up the variable for the onclick overlay
-        const onClickVariable = "updateCardPreview('" + selectedCommanders[i].front + "')";
+        const onClickVariable = "updateCardPreview(getCommanderById('" + selectedCommanders[i].id + "')[0])";
         galleryCard.setAttribute('onclick', onClickVariable);
 
         //create the img tag and append to gallery card div
@@ -97,7 +106,7 @@ function generateFeaturedGallery(gallery, numberOfCards) {
         footerSection.classList.add('footer');
 
         const footerLink = document.createElement('a');
-        footerLink.setAttribute('href', '#');
+        //footerLink.setAttribute('href', '#');
         footerLink.textContent = selectedCommanders[i].name;
         footerSection.appendChild(footerLink);
         galleryCard.appendChild(footerSection);
@@ -109,6 +118,54 @@ function generateFeaturedGallery(gallery, numberOfCards) {
     return gallery;
 }
 
-function updateCardPreview(newSource) {
-    document.getElementById("card-preview").setAttribute("src", newSource);
+function updateCardPreview(commander) {
+
+    if (!commander.back) {
+
+        //create the card-container div
+        let cardContainer = document.createElement('div');
+        cardContainer.setAttribute('class', "card-container");
+
+        //create the img
+        let image = document.createElement('img');
+        image.setAttribute('src', commander.front);
+        cardContainer.appendChild(image);
+
+        document.getElementById("card-preview").replaceChildren(cardContainer);
+    }
+
+    else {
+
+        //create the flip-card div
+        let flipCard = document.createElement('div');
+        flipCard.setAttribute('class', "flip-card");
+
+        //create the flip-card-container div and append to flip-card div
+        let flipCardContainer = document.createElement('div');
+        flipCardContainer.setAttribute('class', "flip-card-container");
+        flipCard.appendChild(flipCardContainer);
+
+        //create the flip-card-front div and append to flip-card-container div
+        let flipCardFront = document.createElement('div');
+        flipCardFront.setAttribute('class', "flip-card-front");
+        flipCardContainer.appendChild(flipCardFront);
+
+        //create the image (front) and append to flip-card-front div
+        let imageFront = document.createElement('img');
+        imageFront.setAttribute('src', commander.front);
+        flipCardFront.appendChild(imageFront);
+
+        //create the flip-card-back div and append to flip-card-container div
+        let flipCardBack = document.createElement('div');
+        flipCardBack.setAttribute('class', "flip-card-back");
+        flipCardContainer.appendChild(flipCardBack);
+
+        //create the image (back) and append to flip-card-back div
+        let imageBack = document.createElement('img');
+        imageBack.setAttribute('src', commander.back);
+        flipCardBack.appendChild(imageBack);
+
+        document.getElementById("card-preview").replaceChildren(flipCard);
+
+    }
 }
